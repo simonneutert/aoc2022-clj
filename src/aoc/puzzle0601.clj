423ce01b5d6ea702b691836cef58144e13e7b36e
Simon Neutert <simonneutert@users.noreply.github.com>
2022-12-06 10:40:48 +0100

    p06

---
+++ src/aoc/puzzle0601.clj
@@ -0,0 +1,21 @@
+(ns aoc.puzzle0601)
+
+(def example ["bvwbjplbgvbhsrlpgdmjqwftvncz"
+              "nppdvjthqldpwncqszvftbrmjlhg"
+              "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+              "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"])
+
+(def puzzle-input (first (clojure.string/split-lines (slurp "resources/puzzle0601.txt"))))
+
+(defn finder
+  [s length]
+  (prn s)
+  (loop
+   [i 0
+    j length]
+    (let [snip (take length (drop i s))]
+      (cond
+        (= (count (set snip)) length) j
+        :else (recur (inc i) (inc j))))))
+
+(finder puzzle-input 4)
